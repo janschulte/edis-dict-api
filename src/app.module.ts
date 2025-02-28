f46957bb39a381950ac2b334bd1bbcd46a4e6a41
Jan Schulte <j.schulte@52north.org>
2025-02-28 09:37:08 +0100

    add request throttling for data harvesting

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,6 @@
-import { HttpModule } from '@nestjs/axios';
+import { HttpModule, HttpService } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 import { NominatimService } from './nominatim/nominatim.service';
 import { OptionsController } from './options/options.controller';
@@ -8,10 +8,11 @@ import { QueryController } from './query/query.controller';
 import { SearchTermListService } from './search-term-list/search-term-list';
 import { StationsService } from './stations/stations.service';
 
+const INTERVAL_MS = 100;
 @Module({
   imports: [
     HttpModule.register({
-      timeout: 5000,
+      // timeout: 5000,
       maxRedirects: 5,
     }),
     ConfigModule.forRoot(),
@@ -19,4 +20,40 @@ import { StationsService } from './stations/stations.service';
   controllers: [QueryController, OptionsController],
   providers: [StationsService, NominatimService, SearchTermListService],
 })
-export class AppModule {}
+export class AppModule {
+  private pendingRequests = 0;
+
+  private readonly maxRequestsCount = this.configService.get<number>(
+    'MAX_REQUESTS_COUNT',
+    99,
+  );
+
+  constructor(
+    private readonly configService: ConfigService,
+    private httpService: HttpService,
+  ) {
+    httpService.axiosRef.interceptors.request.use((config) => {
+      return new Promise((resolve) => {
+        const interval = setInterval(() => {
+          if (this.pendingRequests < this.maxRequestsCount) {
+            this.pendingRequests++;
+            clearInterval(interval);
+            console.log(`Send request: ${config.url}`);
+            resolve(config);
+          }
+        }, INTERVAL_MS);
+      });
+    });
+
+    httpService.axiosRef.interceptors.response.use(
+      (response) => {
+        this.pendingRequests = Math.max(0, this.pendingRequests - 1);
+        return Promise.resolve(response);
+      },
+      (error) => {
+        this.pendingRequests = Math.max(0, this.pendingRequests - 1);
+        return Promise.reject(error);
+      },
+    );
+  }
+}
